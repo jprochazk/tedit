dc456bfb76ffe167f8d624135326a5e51cbbe621
jprochazk <honza.spacir@gmail.com>
2020-11-07 17:18:33 +0100

    Update README, xmake.lua

--- README.md
+++ README.md
@@ -11,4 +11,20 @@
 * Import from and export to (optionally) optimized JSON
 * TypeScript library to efficiently load the maps in HTML5 canvas based games
 
-Uses [`xmake`](https://github.com/xmake-io/xmake).
+## Build & Run
+
+Uses [`xmake`](https://xmake.io/). Please [install](https://xmake.io/#/getting_started?id=installation) it first.
+
+```
+$ xmake
+$ xmake run
+```
+
+### Why XMake?
+
+Because I refuse to deal with CMake's awful syntax, complexity, and slow builds. There are many better alternatives for modern projects.
+
+1. The `xmake` file is in written [Lua](http://www.lua.org/), which is a simple, readable, yet powerful scripting language.
+2. Builds your project [as fast](https://xmake.io/#/getting_started?id=build-as-fast-as-ninja) as the [`ninja`](https://ninja-build.org/) build system
+3. It's also a [package manager](https://github.com/xmake-io/xmake-repo).
+4. It is well maintained, active and popular, with more than 3k stars on [GitHub](https://github.com/xmake-io/xmake).
--- xmake.lua
+++ xmake.lua
@@ -1,93 +1,29 @@
+-- add a debug and release build
 add_rules("mode.debug", "mode.release")
 
+-- bring in dependencies from xmake's own package repository
 add_requires("glfw >=3.3.2")
+-- or from conan (among others)
 add_requires("CONAN::glm/0.9.9.8", { alias = "glm" })
 
+-- create a target
 target("tedit")
     set_kind("binary")
     add_files("src/*.cpp")
     add_includedirs("src")
     add_includedirs("vendor")
 
+    -- include files from any source
     -- glad
     add_files("vendor/glad/glad/glad.c")
     add_includedirs("vendor/glad")
 
-    -- imgui
+    -- imgui using OpenGL3/GLFW backend
     add_files("vendor/imgui/*.cpp")
     add_files("vendor/imgui/backends/imgui_impl_opengl3.cpp")
     add_files("vendor/imgui/backends/imgui_impl_glfw.cpp")
     add_includedirs("vendor/imgui")
 
+    -- and link the dependencies we brought in
     add_packages("glfw")
     add_packages("glm")
-
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro defination
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
-
